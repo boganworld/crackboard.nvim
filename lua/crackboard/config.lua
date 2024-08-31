0beff76f30c34066abbcc36a080bea38c838306c
Rafael de Bem <debemrafael@gmail.com>
2024-08-30 23:53:23 -0300

    feat: remove plenary, general improvements

--- lua/crackboard/config.lua
+++ lua/crackboard/config.lua
@@ -1,9 +1,10 @@
-local config = {}
+local default_config = {
+    BASE_URL = "crackboard.dev",
+    HEARTBEAT = "/heartbeat",
+    HEARTBEAT_INTERVAL = 2 * 60 * 1000, -- 2 minutes in milliseconds
+    session_key = nil,
+    last_heartbeat_time = nil,
+    typing_timer = nil
+}
 
-config.HEARTBEAT_INTERVAL = 2 * 60 * 1000  -- 2 minutes in milliseconds
-config.ENDPOINT = 'https://crackboard.dev/heartbeat'
-config.session_key = nil
-config.last_heartbeat_time = nil
-config.typing_timer = nil
-
-return config
+return default_config
--- lua/crackboard/init.lua
+++ lua/crackboard/init.lua
@@ -1,22 +1,78 @@
-local config = require('crackboard.config')
-local http = require('plenary.http')
+local config = require 'crackboard.config'
 
 local function send_heartbeat(language)
-    local body = vim.fn.json_encode({
+    if not config.session_key then
+        print("Crackboard: Session key not set. Use :CrackboardSetSessionKey to set it.")
+        return
+    end
+
+    local body = vim.json.encode({
         timestamp = os.date('!%Y-%m-%dT%TZ'),
         session_key = config.session_key,
         language_name = language
     })
 
-    http.post(config.ENDPOINT, {
-        headers = { ['Content-Type'] = 'application/json' },
-        body = body
-    }, function(response)
-        if response and response.status ~= 200 then
-            print('Failed to send heartbeat:', response.status)
-        else
-            config.last_heartbeat_time = vim.fn.localtime() * 1000
+    local client = vim.loop.new_tcp()
+    local req = table.concat({
+        string.format("POST %s HTTP/1.1", config.HEARTBEAT),
+        "Host: " .. config.BASE_URL,
+        "Accept: */*",
+        "Content-Type: application/json",
+        "Content-Length: ".. #body,
+        "",
+        body,
+        "",
+    }, "\n")
+
+    print(req)
+
+    vim.loop.getaddrinfo(config.BASE_URL, "443", {
+        family = "inet",
+        socktype = "stream",
+        protocol = "tcp"
+    }, function(err, res)
+        if err then
+            print("DNS resolution error: " .. err)
+            return
         end
+
+        if not res or #res == 0 then
+            print("Failed to resolve " .. config.BASE_URL)
+            return
+        end
+
+        local client = vim.loop.new_tcp()
+        client:connect(res[1].addr, res[1].port, function(err)
+            if err then
+                print("Connection error: " .. err)
+                client:close()
+                return
+            end
+
+            client:write(req)
+
+            local response = ""
+            client:read_start(function(err, chunk)
+                if err then
+                    print("Read error: " .. err)
+                    client:close()
+                    return
+                end
+
+                if chunk then
+                    response = response .. chunk
+                else
+                    client:close()
+                    local status = response:match("HTTP/1%.%d (%d+)")
+                    if status == "200" then
+                        config.last_heartbeat_time = uv.now()
+                        print("Heartbeat sent successfully")
+                    else
+                        print('Failed to send heartbeat. Status: ' .. (status or "unknown"))
+                    end
+                end
+            end)
+        end)
     end)
 end
 
@@ -43,11 +99,14 @@ end
 local M = {}
 
 function M.set_session_key()
-    vim.ui.input({ prompt = 'Enter session key: ', default = '' }, function(input)
+    vim.ui.input({
+        prompt = 'Enter session key: ',
+        default = ''
+    }, function(input)
         if input then
             config.session_key = input
             vim.g.crackboard_session_key = input
-            print('Session key set to: ' .. input)
+            print('Session key set')
         end
     end)
 end
@@ -56,10 +115,22 @@ function M.load_session_key()
     config.session_key = vim.g.crackboard_session_key
 end
 
-function M.setup()
-    M.load_session_key()
-    vim.api.nvim_create_autocmd("BufWritePost", { pattern = "*", callback = on_save })
-    vim.api.nvim_create_autocmd("TextChanged", { pattern = "*", callback = on_change })
+function M.setup(opts)
+    print(opts)
+    
+    config = vim.tbl_deep_extend("force", config, opts)
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*",
+        callback = on_save
+    })
+
+    vim.api.nvim_create_autocmd("TextChanged", {
+        pattern = "*",
+        callback = on_change
+    })
+
+    vim.api.nvim_create_user_command("CrackboardSetSessionKey", M.set_session_key, {})
 end
 
 return M
